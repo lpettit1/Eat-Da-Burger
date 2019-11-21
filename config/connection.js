49560da251dd24c3e5794ac616e83b66313310ec
lpettit1 <50005432+lpettit1@users.noreply.github.com>
2019-11-20 20:32:27 -0700

    mysql to heroku

--- config/connection.js
+++ config/connection.js
@@ -7,16 +7,15 @@ if (process.env.JAWSDB_URL) {
 } else {
 	connection = mysql.createConnection({
         host: 'localhost',
-        port: 3306,
 		user: 'root',
-		password: '',
+		password: 'doh8xz6rev1qtkfi',
 		database: 'burgers_db'
 	});
 };
 
 connection.connect(function(err) {
 	if (err) {
-		console.error('error conencting: ' + err.stack);
+		console.error('error connecting: ' + err.stack);
 		return;
 	}
 	console.log('connected as id ' + connection.threadId);
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,9 +6,7 @@ router.get('/', function(req, res) {
 	res.redirect('/index');
 });
 
-// add a '/index/' endpoint that gets all the burgers
-// then renders the index file by passing in all the burgers
-// as an object for handlebars to use
+
 router.get('/index', function(req, res) {
 	burger.selectAll(function(data) {
 		var hbsObject = {burgers: data};
@@ -17,18 +15,14 @@ router.get('/index', function(req, res) {
 	});
 });
 
-// add a '/burgers/insertOne' endpoint that posts the 
-// burger name the user entered then as a callback it
-// redirects back to the /index route
+
 router.post('/burgers/insertOne', function(req, res) {
 	burger.insertOne(['burger_name', 'devoured'], [req.body.name, false], function() {
 		res.redirect('/index');
 	});
 });
 
-// add a '/burgers/updateOne/:id' route that updates
-// the status of the burger from being uneaten to eaten
-// then does a callback that redirects to the /index endpoint
+
 router.put('/burgers/updateOne/:id', function(req, res) {
 	var condition = 'id = ' + req.params.id;
 	console.log('condition', condition);
@@ -38,5 +32,5 @@ router.put('/burgers/updateOne/:id', function(req, res) {
 	});
 });
 
-// export the router (controller) back to the server
+
 module.exports = router;
